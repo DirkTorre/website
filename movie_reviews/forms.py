a8aefd1a4810d15c440a5e3c20b35a2733cb20a0
DirkTorre <dirkvdtorre@live.nl>
2024-09-24 22:12:22 +0200

    fixed review update form and added css

--- movie_reviews/forms.py
+++ movie_reviews/forms.py
@@ -4,32 +4,32 @@ import re
 
 from .models import MovieStatus, MovieReview
 
-class AddMovieForm(forms.Form):
-     imdb_id = forms.URLField(label="IMDb URL", required=True)
 
-     def clean_imdb_id(self):
+class AddMovieForm(forms.Form):
+    imdb_id = forms.URLField(label="IMDb URL", required=True)
+
+    def clean_imdb_id(self):
         """
         Get the id from the url.
         """
         url = self.cleaned_data["imdb_id"]
-        if 'www.imdb.com/' not in url:
+        if "www.imdb.com/" not in url:
             raise ValidationError("URL does not originate from IMDb.com")
-        if not re.compile('/tt[0-9]+').search(url):
+        if not re.compile("/tt[0-9]+").search(url):
             raise ValidationError("tconst not found")
-        regex = re.compile('^tt[0-9]+')
-        tconst = list(filter(lambda id: regex.match(id), url.split('/')))[0]
+        regex = re.compile("^tt[0-9]+")
+        tconst = list(filter(lambda id: regex.match(id), url.split("/")))[0]
         return tconst
 
 
 class MovieReviewForm(forms.ModelForm):
     watch_date = forms.DateField(
         required=False,
-        widget=forms.DateInput(attrs={"type": "date"})
+        widget=forms.DateInput(format="%d/%m/%Y", attrs={"type": "date"}),
+        input_formats=["%d/%m/%Y"],
     )
-    notes = forms.DateField(
-        required=False,
-        widget=forms.Textarea()
-    )
+    notes = forms.CharField(required=False, widget=forms.Textarea())
+
     class Meta:
         model = MovieReview
-        exclude = ['tconst']
+        exclude = ["tconst"]
